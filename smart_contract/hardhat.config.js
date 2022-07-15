48df9e4547036d8a673be81752eacc20e9acc1be
akshato7 <akshathg7@gmail.com>
2022-07-15 16:55:18 +0530

    first commit

---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+
+require('@nomiclabs/hardhat-waffle')
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/QxQaq3PsbFCFimL7PeMzn0HEz7V1mfPv',
+      accounts:['7874a4931388e4fedbca624e990cbba598bcd7f02380e6eaf8df7edd537a2b64']
+    }
+  }
+ }
